f7011638ad8096447523a8d131dc08b030f0aceb
Povilas <povilas.panavas@gmail.com>
2026-02-09 03:07:33 +0000

    Refactor styles and components to integrate Tailwind CSS, update layout and visuals

--- PandaBattleship.FE/src/App.jsx
+++ PandaBattleship.FE/src/App.jsx
@@ -1,35 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import pandaLogo from './assets/brave_panda_1024.png'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div className="max-w-5xl mx-auto p-8 text-center">
+            <h1>Nothing to see here</h1>
+            <img className="max-w-[500px] max-h-[500px] w-auto h-auto" src={pandaLogo} alt="logo"/>
+        </div>
+    )
 }
 
 export default App

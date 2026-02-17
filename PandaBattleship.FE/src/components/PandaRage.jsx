200436c5b7ba26440b117608e4c3510e3ff78bc6
Povilas <povilas.panavas@gmail.com>
2026-02-17 20:45:24 +0000

    Removed not needed function for hiding end game overlay

--- PandaBattleship.FE/src/components/PandaRage.jsx
+++ PandaBattleship.FE/src/components/PandaRage.jsx
@@ -1,11 +1,11 @@
 ﻿import "./PandaRage.css";
 
-export default function PandaRage({ onComplete }) {
+export default function PandaRage() {
 
     return (
-        <div className="rage-overlay" onClick={onComplete}>
+        <div className="rage-overlay">
             <div className="rage-flash" />
-            <div className="rage-message text-slate-700 text-xl font-bold font-poppins bg-amber-100 font-semibold
+            <div className="rage-message text-slate-700 text-xl font-bold font-poppins bg-amber-100
                 px-2 py-1 gap-2 rounded-full transition-colors drop-shadow-l tracking-wide animate-pulse">
                 MISSION FAILED
             </div>

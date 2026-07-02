cdb5ea0beef1779827847ec499e2f488a6bebdfb
Povilas <povilas.panavas@gmail.com>
2026-07-02 19:18:31 +0100

    Apps deployed and working 🎉🐼

--- iac/outputs.tf
+++ iac/outputs.tf
@@ -7,3 +7,11 @@ output "static_web_app_url" {
   description = "The default HTTPS URL of the Static Web App"
   value       = "https://${azurerm_static_web_app.frontend.default_host_name}"
 }
+
+output "container_app_api_url" {
+  value = "https://${azurerm_container_app.pandabattleship_app_api.ingress[0].fqdn}"
+}
+
+output "container_app_fe_url" {
+  value = "https://${azurerm_container_app.pandabattleship_app_fe.ingress[0].fqdn}"
+}
